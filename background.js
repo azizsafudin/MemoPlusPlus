ddbdfc9309b082cab8157532c765fff3650398f7
xmodulus <azizsafudin@yahoo.com.sg>
2018-06-06 15:54:38 +1200

    Added linking to @handles (only links to the profile search page). Fixed poll bars not appearing on some pages and looking weird on others. Removed Background.js.

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.get("memo-mutelist", function(items) {
-    if(items === null || items === undefined){
-    	chrome.storage.sync.set({"memo-mutelist": []}, function() {
-    		console.log("Loaded new empty list.");
-  		});
-    }
-  });
-});
--- content.js
+++ content.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	muteUsers();
 	fancyPolls();
 	generalChanges();
+	nameTag();
 	$("body").fadeIn(100);		//	Fade in.
 });
 
@@ -9,7 +10,7 @@ function generalChanges(){
 	$('nav.navbar').addClass('navbar-fixed-top');
 	$('div.wrapper').css('padding-top', '60px');
 
-	$('a.navbar-brand').attr('href', 'https://memo.cash/posts/personalized?range=24h');
+	// $('a.navbar-brand').attr('href', 'https://memo.cash/posts/personalized?range=24h');
 
 	$('li a:contains("New")').css('font-weight','bold');
 
@@ -115,7 +116,7 @@ function isMuted(name){
 	Make polls look fancy
 */
 function fancyPolls(){
-	if(window.location.href.indexOf("/polls") > -1 || window.location.href.indexOf("/posts") > -1 ) {
+	if(window.location.href.indexOf("/poll") > -1 || window.location.href.indexOf("/post") > -1 ) {
 		$('tbody').each(function(index){
 			var totalVotes = 0;
 			$(this).children('tr').each(function(index){
@@ -125,12 +126,27 @@ function fancyPolls(){
 			$(this).children('tr').each(function(index){
 				var votes = $(this).children('td').eq(1).text().split(' votes');
 				var percentage = totalVotes != 0 ? Math.round((votes[0]/totalVotes)*100) : 0;
+				var barwidth = totalVotes != 0 ? Math.round((votes[0]/totalVotes)*94) : 0;
+
 				$(this).children('td').css('width', '30%');
 				$(this).children('td').eq(0).after(	'<td><div class="progress memo-poll" style="margin: 0.3em;"><div class="progress-bar progress-bar-striped" '+
-													'style="width: '+(percentage+6)+'%; text-align:left; padding-left:0.4em; font-weight:bold;">'+percentage+'%</div></div></td>');
+													'style="width: '+(barwidth+6)+'%; text-align:left; padding-left:0.4em; font-weight:bold;">'+percentage+'%</div></div></td>');
 				$(this).children('td').css('margin', 'auto');
 				$(this).children('td').css('padding', '0.5em');
 			});
 		});
 	}
+}
+
+/*
+
+
+*/
+
+function nameTag(){
+	$('.message').each(function() {
+		var text = $(this).html();
+        out = text.replace(/@([a-z\d_]+)/ig, '<a href="https://memo.cash/profiles?s=$1">@$1</a>'); 
+        $(this).html(out); 
+    });
 }
