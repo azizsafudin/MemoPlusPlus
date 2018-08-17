904ea57c1671c808102d13d98953f8a2e67c0394
xmodulus <azizsafudin@yahoo.com.sg>
2018-08-17 21:54:51 +1200

    Added changelog

--- src/changelog.js
+++ src/changelog.js
@@ -19,12 +19,16 @@ function showChangelog() {
     toastr.options.onHidden = function() { localStorage.setItem('seen-update', true) }
     if (isLatest && !seen) {
         toastr.success(changelog, '<b>New memo++ features!</b>');
+        toastr.info('FYI: Proofs of default verified accounts can be found on Github!');
     }
 }
 
 const changelog =
     `<ul>
-    	<li>Set font size of posts and topics!</li>
-    	<li>Search posts and topics!</li>
-    	<li>More options in the settings page!</li>
+    	<li>Set default font size of posts.</li>
+    	<li>Search posts and topics.</li>
+    	<li>Browse hashtags latest hashtags.</li>
+    	<li>Get notified of @mentions (only when tab is open).</li>
+    	<li>More options in settings page.</li>
+    	<li>Minor UI tweaks.</li>
     </ul>`
