e4f7c02b02f7e64d4e795b65a4ac6afe74802991
xmodulus <azizsafudin@yahoo.com.sg>
2018-08-17 21:59:52 +1200

    Fixed typo. Cleared toasts on closing any of them.

--- src/changelog.js
+++ src/changelog.js
@@ -16,10 +16,14 @@ function showChangelog() {
         "extendedTimeOut": "0",
         "allowHtml": true
     }
-    toastr.options.onHidden = function() { localStorage.setItem('seen-update', true) }
+    toastr.options.onHidden = function() { 
+    	localStorage.setItem('seen-update', true);
+    	toastr.clear();
+	}
     if (isLatest && !seen) {
         toastr.success(changelog, '<b>New memo++ features!</b>');
         toastr.info('FYI: Proofs of default verified accounts can be found on Github!');
+        toastr.info('Do suggest features or report bugs by making an issue on Github or contacting me (modulus) on memo/twitter.');
     }
 }
 
@@ -27,7 +31,7 @@ const changelog =
     `<ul>
     	<li>Set default font size of posts.</li>
     	<li>Search posts and topics.</li>
-    	<li>Browse hashtags latest hashtags.</li>
+    	<li>Browse latest hashtags.</li>
     	<li>Get notified of @mentions (only when tab is open).</li>
     	<li>More options in settings page.</li>
     	<li>Minor UI tweaks.</li>
